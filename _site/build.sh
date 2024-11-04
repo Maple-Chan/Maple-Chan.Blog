c3f3911cc67450f042d0a9dc7f023450f0dbd7bf
Maple Chan <f310815007@outlook.com>
2024-11-04 23:24:25 +0800

    demo测试

--- _site/build.sh
+++ _site/build.sh
@@ -1,6 +1,6 @@
 # bin/bash.sh
 # 复制文件
 rm -rf ./page/app/randomCheckIn/*
-cp -r /home/maple/GitWorkspace/randomCheckIn/dist/* /home/maple/GitWorkspace/Maple-Chan.Blog/_site/page/randomCheckIn
+cp -r /home/maple/GitWorkspace/randomCheckIn/dist/* /home/maple/GitWorkspace/Maple-Chan.Blog/page/app/randomCheckIn/
 # 编译博客内容
 bundle exec jekyll serve -V
