e794049615e6b422774aa6afa102817b3219c50a
Maple Chan <f310815007@outlook.com>
2024-11-09 12:23:57 +0800

    [deploy]upload

--- _site/build.sh
+++ _site/build.sh
@@ -1,6 +1,14 @@
 # bin/bash.sh
+# 拉取代码
+version=$1
+pwd
+cd ../randomCheckIn/
+git pull
+cd ../Maple-Chan.Blog/
 # 复制文件
 rm -rf ./page/app/randomCheckIn/*
 cp -r /home/maple/GitWorkspace/randomCheckIn/dist/* /home/maple/GitWorkspace/Maple-Chan.Blog/page/app/randomCheckIn/
 # 编译博客内容
 bundle exec jekyll serve -V
+git add .
+git commit -m "[deploy]upload $version"
